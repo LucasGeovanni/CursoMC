c7a94125e8afec25eeded1de6c26a11f993c1dd3
Lucas Geovanni <lucasgeovanni1995@gmail.com>
2018-07-25 22:12:29 -0300

    ajustes conexão banco de dados

--- src/main/java/br/com/lucasgeovanni/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/lucasgeovanni/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package br.com.lucasgeovanni.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lucasgeovanni.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Informática");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+
+		return lista;
 	}
 }
